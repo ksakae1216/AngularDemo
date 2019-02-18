42597d70b2ea0c129470238c4b60179d819fb735
kotaro_sakae <ksakae1216@gmail.com>
2019-02-18 22:02:56 +0900

    Install Angular Material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component'
 import { ListComponent } from './list/list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { ListComponent } from './list/list.component';
     AppRoutingModule,
     FormsModule,
     FlexLayoutModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
